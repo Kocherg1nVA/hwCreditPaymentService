3f32d6213cfd5049e3ef1378ae74ca6bec352669
Vadim Kochergin <kochergin1990vadim@gmail.com>
2024-11-14 19:24:39 +0300

    idiotEvolution

--- src/Credit.java
+++ src/Credit.java
@@ -1,10 +1,7 @@
 public class Credit {
 
     public double calculate(double a, double b, double c) {
-        double d = c / 12 / 100; /* ставка мес - ТУТ должно быть = c/b/100, (b - срок кредита в месяцах),
-                                                 вместо b оставил "12", чтобы сумма ежемесячного платежа была ровно такой,
-                                                 как на скриншотах к задаче
-                                                 */
+        double d = c / 12 / 100; // ставка мес
         double e = Math.pow((1 + d), b);
         double f = a * (d * e) / (e - 1); // ежемесячный платеж
         return (int) f;
